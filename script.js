6e53b5a243355f51fc484dec715cf8b7eabd3184
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-12-05 19:52:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let audio=null;
+function playSound(){
+	stopSound();
+	audio=new Audio();
+	audio.play();
+	
+}
+ function stopSound(){
+	 if(audio!=null){
+		 audio.pause();
+	 }
+		 
+ }
